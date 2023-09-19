fa6ce2b35ee60943864548da907f1e783d93c0c5
GrimdvlLaptop <grimdvl@gmail.com>
2023-09-19 22:42:08 +0300

    first commit

---
+++ src/js/modules/forms.js
@@ -0,0 +1,61 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input'),
+          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+    
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            //Старая проверка которая используеться если нельзя проверить typeOf инпута например если имеем дело с дивом
+            item.value = item.value.replace(/\D/, '');
+        });
+    });       
+
+    const message = {
+          loading: 'Loading...',
+          success: 'Thanks! I will call you',
+          failure: 'Ups! Something wrong...'  
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
